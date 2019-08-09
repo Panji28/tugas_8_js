fc4bacb8cf70d18a8cb823d44125e45985f8607a
Panji28 <panjiprawirowibowo@gmail.com>
2019-08-09 18:20:56 +0700

    Tugas 8 Javascript
    Tugas 8 js

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+function object(){
+var handphone = {
+    merk : 'Nokia',
+    harga : 5000000,
+    warna : 'putih',
+    operating_system : 'android jelly bean',
+
+
+}
+handphone.merk = 'Samsung';
+console.log(handphone)
+}
+
+object()
